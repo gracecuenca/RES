10fc80e56f4f77f085c560b2c919665f517c10ab
Grace Cuenca <gcuenca@princeton.edu>
2019-07-16 13:59:02 -0700

    login and signup methods added

--- app/src/main/java/com/example/fbu_res/LoginActivity.java
+++ app/src/main/java/com/example/fbu_res/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.example.fbu_res;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +18,47 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    private void signUp(String username, String password){
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("LoginActivity", "Sign up successful");
+                    final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Log.e("LoginActivity", "Sign up failed");
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Account with this username already exists", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+    }
+
+    private void login(String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e == null){ // if there are no errors
+                    Log.d("LoginActivity", "Login successful");
+                    final Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish(); // dont want the user to log out with a simple back press
+                }else{
+                    Log.e("LoginActivity", "Login failed");
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
