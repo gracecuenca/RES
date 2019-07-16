b8f376567e5a4677a4b43b7ed4ff451a2a21367d
Grace Cuenca <gcuenca@princeton.edu>
2019-07-16 14:27:32 -0700

    home activity - bottom nav bar skeleton

--- app/src/main/java/com/example/fbu_res/HomeActivity.java
+++ app/src/main/java/com/example/fbu_res/HomeActivity.java
@@ -1,14 +1,50 @@
 package com.example.fbu_res;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+
+    // bottom navigation bar setup
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // do findByVeiwId of the bottom navigation bar here
+        // bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        /*
+        // bottom navigation bar setup continued
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment = new PostsFragment();
+                        break;
+                    case R.id.action_compose:
+                        fragment = new ComposeFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
+         */
+
     }
 }
