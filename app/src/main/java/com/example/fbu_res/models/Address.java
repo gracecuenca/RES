6bac7ce2762092f190e616a94e776220b23b40b4
ayyolmao <adeoyeayomide11@gmail.com>
2019-07-16 14:58:29 -0700

    Address class

--- app/src/main/java/com/example/fbu_res/models/Address.java
+++ app/src/main/java/com/example/fbu_res/models/Address.java
@@ -1,50 +1,73 @@
 package com.example.fbu_res.models;
 
 import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 public class Address extends ParseObject {
-    public static final String KEY_DESCRIPTION = "description";
-    public static final String KEY_IMAGE = "image";
-    public static final String KEY_USER = "user";
-    public static final String KEY_TIMESTAMP = "createdAt";
-    public static final String KEY_OBJECT_ID = "objectId";
+    public static final String KEY_ADDRESSLINE1 = "addressLineOne";
+    public static final String KEY_ADDRESSLINE2 = "addressLineTwo";
+    public static final String KEY_CITY = "city";
+    public static final String KEY_STATE = "state";
+    public static final String KEY_ZIPCODE = "zipcode";
+    public static final String KEY_COUNTRY = "country";
+    public static final String KEY_PIN = "pin";
 
-    public String getKeyDescription() {
-        return getString(KEY_DESCRIPTION);
+
+    public String getKeyAddressline1() {
+        return getString(KEY_ADDRESSLINE1);
     }
 
-
-    public String getKeyObjectId() {
-        return getObjectId();
+    public void setKeyAddressline1(String address) {
+        put(KEY_ADDRESSLINE1, address);
     }
 
-    public void setKeyDescription(String description) {
-        put(KEY_DESCRIPTION, description);
+    public String getKeyAddressline2() {
+        return getString(KEY_ADDRESSLINE2);
     }
 
-    public ParseFile getKeyImage() {
-        return getParseFile(KEY_IMAGE);
+    public void setKeyAddressline2(String address) {
+        put(KEY_ADDRESSLINE2, address);
     }
 
-    public void setKeyImage(ParseFile image) {
-        put(KEY_IMAGE, image);
+    public String getKeyCity() {
+        return getString(KEY_CITY);
     }
 
-    public ParseUser getKeyUser() {
-        return getParseUser(KEY_USER);
+    public void setKeyCity(String city) {
+        put(KEY_CITY, city);
     }
 
-    public void setKeyUser(ParseUser user) {
-        put(KEY_USER, user);
+    public String getKeyState() {
+        return getString(KEY_STATE);
     }
 
-    public String getKeyTimestamp() {
-        return getString(KEY_TIMESTAMP);
+    public void setKeyState(String state) {
+        put(KEY_STATE, state);
     }
 
-    public void setKeyTimestamp(String timestamp) {
-        put(KEY_TIMESTAMP, timestamp);
+    public int getKeyZipcode() {
+        return getNumber(KEY_ZIPCODE).intValue();
+    }
+
+    public void setKeyZipcode(String address) {
+        put(KEY_ZIPCODE, address);
+    }
+
+    public String getKeyCountry() {
+        return getString(KEY_COUNTRY);
+    }
+
+    public void setKeyCountry(String country) {
+        put(KEY_COUNTRY, country);
+    }
+
+    public ParseGeoPoint getKeyPin() {
+        return getParseGeoPoint(KEY_PIN);
+    }
+
+    public void setKeyPin(ParseGeoPoint pin) {
+        put(KEY_PIN, pin);
     }
 }
