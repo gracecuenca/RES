132e78a819b42f151e21606b2dcf9d3fa2803048
Grace Cuenca <gcuenca@princeton.edu>
2019-07-16 17:11:42 -0700

    home screen, nav bar, fragments set up

--- app/src/main/java/com/example/fbu_res/HomeActivity.java
+++ app/src/main/java/com/example/fbu_res/HomeActivity.java
@@ -1,10 +1,16 @@
 package com.example.fbu_res;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.fbu_res.fragments.GroupFragment;
+import com.example.fbu_res.fragments.HomeFragment;
+import com.example.fbu_res.fragments.ProfileFragment;
+import com.example.fbu_res.fragments.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -18,21 +24,21 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         final FragmentManager fragmentManager = getSupportFragmentManager();
 
-        // do findByVeiwId of the bottom navigation bar here
-        // bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
-        /*
-        // bottom navigation bar setup continued
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 Fragment fragment = null;
                 switch (menuItem.getItemId()) {
                     case R.id.action_home:
-                        fragment = new PostsFragment();
+                        fragment = new HomeFragment();
                         break;
-                    case R.id.action_compose:
-                        fragment = new ComposeFragment();
+                    case R.id.action_search:
+                        fragment = new SearchFragment();
+                        break;
+                    case R.id.action_groups:
+                        fragment = new GroupFragment();
                         break;
                     case R.id.action_profile:
                         fragment = new ProfileFragment();
@@ -44,7 +50,6 @@ public class HomeActivity extends AppCompatActivity {
         });
         // Set default selection
         bottomNavigationView.setSelectedItemId(R.id.action_home);
-         */
 
     }
 }
---
+++ app/src/main/java/com/example/fbu_res/fragments/HomeFragment.java
@@ -0,0 +1,6 @@
+package com.example.fbu_res.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+}
