52ae8023e461f312b8872046a3892646c0f28b32
Grace Cuenca <gcuenca@princeton.edu>
2019-07-17 16:19:48 -0700

    started on detail view

--- app/src/main/java/com/example/fbu_res/EventsAdapter.java
+++ app/src/main/java/com/example/fbu_res/EventsAdapter.java
@@ -1,6 +1,7 @@
 package com.example.fbu_res;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.fbu_res.models.Event;
 import com.parse.ParseFile;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -62,7 +65,19 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-
+                    // gets item position
+                    int position = getAdapterPosition();
+                    // make sure the position is valid, i.e. actually exists in the view
+                    if (position != RecyclerView.NO_POSITION) {
+                        // get the movie at the position, this won't work if the class is static
+                        Event post = events.get(position);
+                        // create intent for the new activity
+                        Intent intent = new Intent(context, EventDetailsActivity.class);
+                        // serialize the movie using parceler, use its short name as a key
+                        intent.putExtra(Event.class.getSimpleName(), Parcels.wrap(post));
+                        // show the activity
+                        context.startActivity(intent);
+                    }
                 }
             });
         }
