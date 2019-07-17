8123ccff2030ba5fc55ec90b6d16c39f9f5b9ff8
ayyolmao <adeoyeayomide11@gmail.com>
2019-07-17 14:52:11 -0700

    Basics for groups

---
+++ app/src/main/java/com/example/fbu_res/fragments/EventGroupFragment.java
@@ -0,0 +1,29 @@
+package com.example.fbu_res.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.fbu_res.R;
+
+public class EventGroupFragment extends Fragment {
+
+    public static GroupFragment newInstance(int page) {
+        return new GroupFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_event_groups, container, false);
+        return view;
+    }
+}
