4ac72e8a855d7187a7e0b5ebbcb4b535ed4ca090
as3484 <aasthashah999@gmail.com>
2019-07-18 14:10:50 -0700

    resolving merge conflict

--- app/src/androidTest/java/com/example/fbu_res/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/fbu_res/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.fbu_res;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
