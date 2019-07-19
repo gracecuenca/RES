2530221b3aeeac9a428115023806e01b6e936f9b
ayyolmao <adeoyeayomide11@gmail.com>
2019-07-19 13:47:00 -0700

    Groups are working but not working.

---
+++ app/src/main/java/com/example/fbu_res/MessageListRowUI.java
@@ -0,0 +1,9 @@
+package com.example.fbu_res;
+
+import android.widget.TextView;
+
+public class MessageListRowUI {
+    public TextView sender;
+    public TextView message;
+    public TextView timestamp;
+}
