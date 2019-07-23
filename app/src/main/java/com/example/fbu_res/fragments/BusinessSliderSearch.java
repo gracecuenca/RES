cc2270ea1a8689eaf2d1cb2a1a9a82705a438936
as3484 <aasthashah999@gmail.com>
2019-07-23 16:34:22 -0700

    comitting working searchViews that filters

--- app/src/main/java/com/example/fbu_res/fragments/BusinessSliderSearch.java
+++ app/src/main/java/com/example/fbu_res/fragments/BusinessSliderSearch.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.fbu_res.R;
+import com.example.fbu_res.adapters.SearchAdapter;
+
+import java.util.ArrayList;
 
 public class BusinessSliderSearch extends Fragment {
+    RecyclerView businessRv;
+    SearchAdapter adapter;
+    public static ArrayList<String> businesses;
+    SearchView businessSearchView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,6 +31,30 @@ public class BusinessSliderSearch extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        businessRv = view.findViewById(R.id.businessRv);
+        businesses = new ArrayList<>();
+        businesses.add("Starbucks");
+        businesses.add("Jamba Juice");
+        businesses.add("Pressed Juicery");
+        adapter = new SearchAdapter(getContext(), new BusinessSliderSearch());
+        businessRv.setAdapter(adapter);
+        LinearLayoutManager manager = new LinearLayoutManager(getContext());
+        businessRv.setLayoutManager(manager);
+        businessSearchView = view.findViewById(R.id.businessSearchView);
+
+        businessSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.filter(newText);
+                return false;
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/fbu_res/fragments/SearchSlider.java
+++ app/src/main/java/com/example/fbu_res/fragments/SearchSlider.java
@@ -2,24 +2,40 @@ package com.example.fbu_res.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.fbu_res.R;
 import com.example.fbu_res.adapters.GroupFragmentPagerAdapter;
+import com.example.fbu_res.adapters.SearchAdapter;
 import com.example.fbu_res.adapters.SearchSliderAdapter;
 import com.google.android.material.tabs.TabLayout;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Locale;
+
 public class SearchSlider extends Fragment{
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -33,9 +49,27 @@ public class SearchSlider extends Fragment{
         SearchSliderAdapter pagerAdapter = new SearchSliderAdapter(getFragmentManager(),
                 getContext());
         viewPager.setAdapter(pagerAdapter);
-
         TabLayout tabLayout = (TabLayout) view.findViewById(R.id.searchSliderTab);
         tabLayout.setupWithViewPager(viewPager);
-
     }
+
+    /*The main list holding the event names and business name will be
+    * be stored in this fragment. Whenever someone types in a letter, the array
+    * will be filtered and sorted accordingly and sent to the correct slider function. In
+    * events search fragment, the updated array will be delivered and the adapter will be notified.
+    * Currently, we are only working with dummy data. There will be a conditional to determine
+    * which fragment the data should go to.*/
+
+    //public void filter(String charText) {
+        //charText = charText.toLowerCase(Locale.getDefault());
+        //ArrayList<String> filteredEvent = new ArrayList<>();
+        //for(int i =0; i < events.size(); i++){
+            //if(events.get(i).toLowerCase(Locale.getDefault()).contains(charText)){
+                //filteredEvent.add(events.get(i));
+            //}
+        //}
+
+    //}
+
+
 }
