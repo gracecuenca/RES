b37950d666a9ee198e2089bda5d9e33a85fda65f
Grace Cuenca <gcuenca@princeton.edu>
2019-07-23 17:51:27 -0700

    sorting by location is slow but functioning

--- app/src/main/java/com/example/fbu_res/GeofenceRegistrationService.java
+++ app/src/main/java/com/example/fbu_res/GeofenceRegistrationService.java
@@ -9,7 +9,7 @@ import com.google.android.gms.location.GeofencingEvent;
 
 import java.util.List;
 
-import static com.example.fbu_res.fragments.HomeFragment.GEOFENCE_ID_STAN_UNI;
+//import static com.example.fbu_res.fragments.HomeFragment.GEOFENCE_ID_STAN_UNI;
 
 public class GeofenceRegistrationService extends IntentService {
 
@@ -28,11 +28,13 @@ public class GeofenceRegistrationService extends IntentService {
             int transaction = geofencingEvent.getGeofenceTransition();
             List<Geofence> geofences = geofencingEvent.getTriggeringGeofences();
             Geofence geofence = geofences.get(0);
+            /*
             if (transaction == Geofence.GEOFENCE_TRANSITION_ENTER && geofence.getRequestId().equals(GEOFENCE_ID_STAN_UNI)) {
                 Log.d(TAG, "You are inside Stanford University");
             } else {
                 Log.d(TAG, "You are outside Stanford University");
             }
+            */
         }
     }
 }
